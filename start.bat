01c5a0125775afa82a2dcaf4a88b7f46f6690a05
SuryaTonyStark <59388153+SuryaTonyStark@users.noreply.github.com>
2021-06-10 00:28:01 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,6 +16,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: SuryaTonyStark
-echo Password: SuryaTonyStark
+echo Password: SuryaSTK@12345
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
